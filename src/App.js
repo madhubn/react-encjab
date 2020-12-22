0e3bbcec102a3b466aaff5da4250db33dfcca9ac
madhubn <madhubn.17@gmail.com>
2020-12-22 02:43:49 -0800

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Hello StackBlitz!</h1>
+//       <p>Start editing to see some magic happen :)</p>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
 }
+export default App;
