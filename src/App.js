9af6afdd1f60f9ff94021ba2c944ae9172297cb6
madhubn <madhubn.17@gmail.com>
2020-12-22 02:47:42 -0800

    1 file modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-
+import { Router, Route, Link, browserHistory, IndexRoute } from "react-router";
 // export default function App() {
 //   return (
 //     <div>
@@ -13,10 +13,14 @@ import "./style.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="home" component={Home} />
+          <Route path="about" component={About} />
+          <Route path="contact" component={Contact} />
+        </Route>
+      </Router>
     );
   }
 }
---
+++ src/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Contact extends React.component {
+  render() {
+    return "Hello, Hi Madhu";
+  }
+}
+
+export default Contact;
