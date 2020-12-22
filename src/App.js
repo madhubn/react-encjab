2a0160d6cb4ae5a1ddfd801ecac05d2a10235632
madhubn <madhubn.17@gmail.com>
2020-12-22 02:59:18 -0800

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./style.css";
-import { Router, Route, Link, browserHistory, IndexRoute } from "react-router";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Contact from "./Contact";
+
 // export default function App() {
 //   return (
 //     <div>
@@ -13,13 +16,36 @@ import { Router, Route, Link, browserHistory, IndexRoute } from "react-router";
 class App extends React.Component {
   render() {
     return (
-      <Router history={browserHistory}>
-        <Route path="/" component={App}>
-          <IndexRoute component={Home} />
-          <Route path="home" component={Home} />
-          <Route path="about" component={About} />
-          <Route path="contact" component={Contact} />
-        </Route>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
       </Router>
     );
   }
--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Contact extends React.component {
+class Contact extends React.Component {
   render() {
     return "Hello, Hi Madhu";
   }
